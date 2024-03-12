c159f3e9d26237fc52606d6ee72a90a35c417958
AMADOR20 <158489746+AMADOR20@users.noreply.github.com>
2024-03-12 08:38:03 -0700

    Update Nodo.java

--- miPrincipal/Nodo.java
+++ miPrincipal/Nodo.java
@@ -1,4 +1,22 @@
 package miPrincipal;
 public class Nodo<T>{
-    
+    private T valor;
+    Nodo<T> siguente;
+    public Nodo(){
+        valor=null;
+        siguente=null;    
+    }
+public T getValor(){
+    return valor;
+}
+public void setValor(T valor){
+    this.valor=valor;
+}
+public Nodo<T> getSiguente(){
+    return siguente;
+}
+
+public void setSiguente(Nodo<T> siguente){
+        this.siguente=siguente;
+    }
 }
