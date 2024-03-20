ea632a5b0f1b937aaa265eb58fc5da5914c7b9eb
AMADOR20 <158489746+AMADOR20@users.noreply.github.com>
2024-03-20 08:35:05 -0700

    Update DemoCola.java

--- miPrincipal/DemoCola.java
+++ miPrincipal/DemoCola.java
@@ -1,9 +1,35 @@
-package miPrincipal 
+package miPrincipal;
 
-public class DemoCola{
-public static void menu{
-system.out.println("******************************");
-system.out.println() 
-}
-  
+public class DemoCola {
+    public static void menu() {
+        System.out.println("****************");
+        System.out.println("Cola");
+        System.out.println("****************");
+        System.err.println();
+      
+        Cola<Integer> cola = new Cola<Integer>();
+      
+        cola.encolar(12);
+        cola.encolar(13);
+        cola.encolar(14);
+        cola.encolar(15);
+   
+        System.out.println("el primer elemento de la cola: "
+           +cola.frente().toString());
+
+        cola.desencolar();
+        System.out.println("el primer elemento de la cola: "
+           +cola.frente().toString());
+        
+        cola.desencolar();
+        System.out.println("el primer elemento de la cola: "
+              +cola.frente().toString());
+
+        cola.desencolar();
+        System.out.println("el primer elemento de la cola: "
+        +cola.frente().toString());
+        cola.desencolar();
+        System.out.println("el primer elemento de la cola: "
+        +cola.frente());
+    }    
 }
