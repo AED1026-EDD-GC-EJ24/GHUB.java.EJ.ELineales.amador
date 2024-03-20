40262bad060040b1c256fdff6e0e949eb6550b27
AMADOR20 <158489746+AMADOR20@users.noreply.github.com>
2024-03-20 08:50:25 -0700

    Create DemoQueue.java

---
+++ miPrincipal/DemoQueue.java
@@ -0,0 +1,16 @@
+package miPrincipal 
+  import java.util.*;
+public class DemoQueue{
+  public static void menu() {
+        System.out.println("****************");
+        System.out.println("Queue");
+        System.out.println("****************");
+        System.err.println();
+
+    Queue miCola=new LinkedList();
+
+    miCola.add("elemento 0");
+    miCola.add("elemento 1");
+    miCola.add("elemento 2");
+  }
+  }
