c9d425adbfc5b7ad5bef43fa38a455a1e864aef5
AMADOR20 <158489746+AMADOR20@users.noreply.github.com>
2024-03-21 08:19:13 -0700

    Create DemoTabla.java

---
+++ miPrincipal/DemoTabla.java
@@ -0,0 +1,12 @@
+package miPrincipal;
+
+public class DemoTabla{
+   public static void menu() {
+        System.out.println("****************");
+        System.out.println("Tabla de dispersion");
+        System.out.println("****************");
+        System.err.println();
+
+     
+   }
+}
