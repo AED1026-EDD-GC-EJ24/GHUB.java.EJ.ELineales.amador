0eda72c5b5674df72ce9692eca059516c7562df4
AMADOR20 <158489746+AMADOR20@users.noreply.github.com>
2024-03-21 08:26:24 -0700

    Create Persona.java

---
+++ miPrincipal/Persona.java
@@ -0,0 +1,21 @@
+package miPrincipal
+  public class Persona{
+    private String nombre;
+    private int edad;
+    private String nacionalidad;
+
+    public class Persona{
+  public String getNombre(){
+    
+  }
+      public int getEdad(){
+      
+    
+  }
+   public String getNacionalidad{
+     return nacionalidad;
+   }   
+    }
+
+    
+  }
